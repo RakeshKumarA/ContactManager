598c54a3f99e5ca129abb6503ef6ad38a6deefab
RakeshKumarA <rkakumar85@gmail.com>
2019-09-06 22:22:39 +0200

    Initial Commit

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John Doe</h4>
+        <ul>
+          <li>Email: john.doe@gmail.com</li>
+          <li>Contact: 555-555-5555</li>
+        </ul>
+        <h4>Rakesh</h4>
+        <ul>
+          <li>Email: rkakumar85@gmail.com</li>
+          <li>Contact: 444-555-5555</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
