c277cd1de6570ad55dbda6422cf73181aa1de0d7
RakeshKumarA <rkakumar85@gmail.com>
2019-09-07 11:58:08 +0200

    Commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 class Contact extends Component {
+  state = {};
+
+  onShowClick = () => {
+    console.log(this.state);
+  };
   render() {
     const { name, email, phone } = this.props.contact;
+    const element = <FontAwesomeIcon icon={faSortDown} />;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>
+          {name}
+          <i onClick={this.onShowClick}>{element}</i>
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
