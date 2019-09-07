8fcc2979c3bd8bc99265616758182bc8281b2ee4
RakeshKumarA <rkakumar85@gmail.com>
2019-09-07 10:18:01 +0200

    New commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager" />
-      <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-      <Contact name="Kane Smith" email="kane@gmail.com" phone="333-333-3333" />
+      <div className="container">
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact
+          name="Kane Smith"
+          email="kane@gmail.com"
+          phone="333-333-3333"
+        />
+      </div>
     </div>
   );
 }
