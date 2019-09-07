9c9188896d2defb4e5c59877d6eb5dfd67540bf5
RakeshKumarA <rkakumar85@gmail.com>
2019-09-07 16:45:35 +0200

    New commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,33 +2,58 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 class Contact extends Component {
-  state = {};
-
-  onShowClick = () => {
-    console.log(this.state);
+  state = {
+    showContactInfo: false
   };
+
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
-    const element = <FontAwesomeIcon icon={faSortDown} />;
+    const sortdownicon = <FontAwesomeIcon icon={faSortDown} />;
+    const crossicon = <FontAwesomeIcon icon={faTimes} />;
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>
           {name}
-          <i onClick={this.onShowClick}>{element}</i>
+          <i
+            onClick={() =>
+              this.setState({
+                showContactInfo: !this.state.showContactInfo
+              })
+            }
+            style={{ cursor: 'pointer' }}
+          >
+            {sortdownicon}
+          </i>
+          <i
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+            onClick={this.onDeleteClick}
+          >
+            {crossicon}
+          </i>
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
